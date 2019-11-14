9b40f434af950f24320308b5f425bf5b2acf6644
Caitlin Potter <caitpotter88@gmail.com>
2019-11-14 14:21:55 -0500

    Add a workaround in the travis script while jscoverage is broken

--- tools/travis.sh
+++ tools/travis.sh
@@ -3,15 +3,17 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/.."
 
-node_modules/.bin/jscoverage lib
+# FIXME: jscoverage fails to exclude the type definition file, which is breaking coverage.
+# Re-enable coverage after https://github.com/fishbar/jscoverage/pull/45 is merged
+#node_modules/.bin/jscoverage lib
 
 # TODO: Only run tests once, and use a custom reporter which outputs both LCOV and text,
 # for Travis.
 node_modules/.bin/mocha test
 STATUS=$?
 
-COVERAGE=1 node_modules/.bin/mocha -R mocha-lcov-reporter test > .lcov
+#COVERAGE=1 node_modules/.bin/mocha -R mocha-lcov-reporter test > .lcov
 
-cat .lcov | node_modules/.bin/coveralls
+#cat .lcov | node_modules/.bin/coveralls
 
 exit $STATUS
