eff8946b8a02f7cff39c9199edd83676ccd15c5f
Caitlin Potter <caitpotter88@gmail.com>
2019-11-14 12:58:24 -0500

    Update travis script

--- tools/travis.sh
+++ tools/travis.sh
@@ -3,8 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/.."
 
-export COVERAGE=1
-
 node_modules/.bin/jscoverage lib
 
 # TODO: Only run tests once, and use a custom reporter which outputs both LCOV and text,
@@ -12,7 +10,7 @@ node_modules/.bin/jscoverage lib
 node_modules/.bin/mocha test
 STATUS=$?
 
-node_modules/.bin/mocha -R mocha-lcov-reporter test > .lcov
+COVERAGE=1 node_modules/.bin/mocha -R mocha-lcov-reporter test > .lcov
 
 cat .lcov | node_modules/.bin/coveralls
 
